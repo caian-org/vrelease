0f5ada22ec178e0786fa19c984041b7445454fce
Caian R. Ertl <hi@caian.org>
2021-03-28 19:34:07 -0300

    ci: init travis-ci

---
+++ .ci/install-v.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+VLANG_TARGET_OS=""
+VLANG_VERSION="0.2.2"
+
+case "$TRAVIS_OS_NAME" in
+    osx) VLANG_TARGET_OS="macos" ;;
+    linux) VLANG_TARGET_OS="linux" ;;
+    windows) VLANG_TARGET_OS="windows" ;;
+esac
+
+FN="v_${VLANG_TARGET_OS}.zip"
+
+wget "https://github.com/vlang/v/releases/download/${VLANG_VERSION}/${FN}"
+unzip "$FN"
+
+cd v
+sudo ./v symlink
