f7c027f33b24d22e5b2f534cf9a0be5f559e71fe
Caian R. Ertl <hi@caian.org>
2021-03-28 20:21:20 -0300

    ci: handle symlinking differently for each os, install dev libs

--- .ci/install-v.sh
+++ .ci/install-v.sh
@@ -24,5 +24,9 @@ echo "Downloading from: $URL"
     ls -lash
 
     cd v
-    ./v symlink
+    ./v up
+
+    if [ "$VLANG_TARGET_OS" = "macos" ];   then ./v symlink; fi
+    if [ "$VLANG_TARGET_OS" = "windows" ]; then ./v symlink; fi
+    if [ "$VLANG_TARGET_OS" = "linux" ];   then sudo ./v symlink; fi
 )
