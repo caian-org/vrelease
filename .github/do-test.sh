2c4308c8e8404142d6728b5a10263d6d5cedec84
Caian R. Ertl <hi@caian.org>
2021-04-10 20:05:31 -0300

    chore: sleep between tests

--- .github/do-test.sh
+++ .github/do-test.sh
@@ -5,23 +5,29 @@ set -ex
 # ....
 ./committer
 ./vrelease
+sleep 5
 
 # ....
 ./committer
 ./vrelease -d
+sleep 5
 
 # ....
 ./committer
 ./vrelease -l 5
+sleep 5
 
 # ....
 ./committer
 ./vrelease -d -n
+sleep 5
 
 # ....
 ./committer
 ./vrelease -a committer
+sleep 5
 
 # ....
 ./committer
 ./vrelease -a committer -a vrelease -d -l 8
+sleep 5
