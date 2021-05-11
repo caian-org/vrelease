925cc6b7ef42c13d15648ab3225e4e2337038d51
Caian R. Ertl <hi@caian.org>
2021-05-11 09:08:55 -0300

    ci: simplify testing, pass required environment variable to docker container

--- .github/do-docker-test.sh
+++ .github/do-docker-test.sh
@@ -2,42 +2,17 @@
 
 set -ex
 
-# ....
-./committer
-docker run vrelease
-./vrelease
-sleep 5
+run_test() {
+    ./committer
+    docker run -e VRELEASE_AUTH_TOKEN="$TOKEN" vrelease $@
+    sleep 5
+}
 
-# ....
-./committer
-docker run vrelease -d
-sleep 5
-
-# ....
-./committer
-docker run vrelease -l 5
-sleep 5
-
-# ....
-./committer
-docker run vrelease -d -n
-sleep 5
-
-# ....
-./committer
-docker run vrelease -a committer
-sleep 5
-
-# ....
-./committer
-docker run vrelease -c -a committer
-sleep 5
-
-# ....
-./committer
-docker run vrelease -a committer -a vrelease -d -l 8
-sleep 5
-
-# ....
-./committer
-docker run vrelease -i -c -a committer
+run_test
+run_test -d
+run_test -l 5
+run_test -d -n
+run_test -a committer
+run_test -c -a committer
+run_test -a committer -a vrelease -d -l 8
+run_test -i -c -a committer
