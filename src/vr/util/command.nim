fe4cf094c386f8b0e818ff59dc4d12c11fedf345
Caian Ertl <hi@caian.org>
2021-12-04 03:11:10 -0300

    fix: string interpolation

--- src/vr/util/command.nim
+++ src/vr/util/command.nim
@@ -1,11 +1,12 @@
 import std/osproc
+import std/strformat
 
 
 proc execOrPanic*(cmd: string): (string, int) =
   let (output, exitCode) = execCmdEx(cmd)
 
   if exitCode > 0:
-    let co = if len(output) > 0: "; got: \n\n{output}" else: ""
-    raise newException(Defect, "command '{cmd}' failed with return code {exitCode}" & co)
+    let co = if len(output) > 0: &"; got: \n\n{output}" else: ""
+    raise newException(Defect, &"command '{cmd}' failed with return code {exitCode} {co}")
 
   return (output, exitCode)
