c94f30b1c6693feb4e0088fba7f4297b10ce4ec3
Caian Ertl <hi@caian.org>
2021-12-19 05:27:13 -0300

    fix: code cleanup

--- src/vr/util/str.nim
+++ src/vr/util/str.nim
@@ -1,29 +1,27 @@
 import std/terminal
 
 
+func first* (t: seq[string]): string = t[0]
+
+func last* (t: seq[string]): string = t[len(t) - 1]
+
 func toColor (t: string, color: ForegroundColor, bright: bool): string =
-  return ansiForegroundColorCode(color, bright = bright) & t & ansiResetCode
+  ansiForegroundColorCode(color, bright = bright) & t & ansiResetCode
 
 func toBrightBlueColor* (t: string): string =
-  return t.toColor(fgBlue, true)
+  t.toColor(fgBlue, true)
 
 func toBrightRedColor* (t: string): string =
-  return t.toColor(fgRed, true)
+  t.toColor(fgRed, true)
 
 func toDimStyle* (t: string): string =
-  return ansiStyleCode(styleDim) & t & ansiResetCode
+  ansiStyleCode(styleDim) & t & ansiResetCode
 
 func toBoldStyle* (t: string): string =
-  return ansiStyleCode(styleBright) & t & ansiResetCode
+  ansiStyleCode(styleBright) & t & ansiResetCode
 
 func atRelativeIndex* (t: seq[string], i: int): string =
   if i >= 0:
     return t[i]
 
   return t[len(t) + i]
-
-func first* (t: seq[string]): string =
-  return t[0]
-
-func last* (t: seq[string]): string =
-  return t[len(t) - 1]
